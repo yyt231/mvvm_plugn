2bac2a8a1e3e907af078790a8346bdb5d9014ed2
GitHub Action <action@github.com>
2022-02-22 16:08:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/yyt231/mvvmplugn/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/yyt231/mvvmplugn/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yyt231.mvvmplugn.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yyt231.mvvmplugn.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/yyt231/mvvmplugn/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yyt231/mvvmplugn/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yyt231.mvvmplugn.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yyt231.mvvmplugn.MyBundle
 
 class MyApplicationService {
 
